e6aa09d4df507dd30c3fc23fff8fb9cd66b45997
Kevin Fung <kfung@alegion.com>
2020-04-21 22:57:44 -0500

    GREEN: controller advice mapping entity not found exception correctly to http status 404

---
+++ src/main/java/com/example/bowling/exception/RestControllerExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.bowling.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+
+@ControllerAdvice
+public class RestControllerExceptionHandler extends ResponseEntityExceptionHandler {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(EntityNotFoundException.class)
+    public void handleNotFound() {
+        // Nothing to do
+    }
+}
