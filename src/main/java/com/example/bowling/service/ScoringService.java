e435b9a15e3c45c9cda2b7bf92f7f379d7ffcbf0
Kevin Fung <kfung@alegion.com>
2020-04-23 16:53:04 -0500

    GREEN: can now add spares

--- src/main/java/com/example/bowling/service/ScoringService.java
+++ src/main/java/com/example/bowling/service/ScoringService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.example.bowling.common.Constants.MAX_PINS;
+
 @Service
 public class ScoringService {
     public PlayerEntity calculateScore(PlayerEntity playerEntity) {
@@ -14,10 +16,20 @@ public class ScoringService {
         int score = getLastRecordedScore(frameEntities, lastScoredFrame);
         for(int i = lastScoredFrame; i < frameEntities.size(); i++) {
             FrameEntity frameEntity = frameEntities.get(i);
-            score += frameEntity.getRolls()[0] + frameEntity.getRolls()[1];
-            frameEntity.setFrameScore(score);
+            if(frameEntity.isSpare()) {
+                if(i + 1 >= frameEntities.size()) {
+                    break;
+                }
+                score += MAX_PINS + frameEntities.get(i + 1).getRolls()[0];
 
-            playerEntity.setLastScoredFrame(playerEntity.getLastScoredFrame() + 1);
+                frameEntity.setFrameScore(score);
+                playerEntity.setLastScoredFrame(playerEntity.getLastScoredFrame() + 1);
+            } else {
+                score += frameEntity.getRolls()[0] + frameEntity.getRolls()[1];
+
+                frameEntity.setFrameScore(score);
+                playerEntity.setLastScoredFrame(playerEntity.getLastScoredFrame() + 1);
+            }
         }
         return playerEntity;
     }
